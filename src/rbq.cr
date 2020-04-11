cb22708feb93c2b0101a26d89d269bfc37b579fe
D. Scott Boggs <scott@tams.tech>
2020-04-11 11:46:34 -0400

    Reorganize code

--- src/rbq.cr
+++ src/rbq.cr
@@ -9,52 +9,15 @@ module CSP
     end
   end
 
-  struct RBQ(T, I)
-    def initialize(capacity_exponent)
-      @capacity = 1 << capacity_exponent
-      @mask = cap - 1
-      @items : Items(T, I) = Items(T, I).new self, @capacity
-    end
-
-    property items,
-      capacity : LibC::SizeT,
-      mask : LibC::SizeT,
-      slow = 0_u64,
-      fast = 0_u64
-
-    # Returns true if successful
-    def try_push(item : T) : Bool
-      !!if size < capacity
-        items[@slow] = item
-        @slow += 1
-      end
-    end
-
-    # Returns true if successful
-    def try_push_front(item : T) : Bool
-      !!if size < capacity
-        @items[@slow -= 1] = item
-      end
-    end
-
-    def try_pop : T?
-      if size > 0
-        @items[@slow].tap { @slow += 1 }
-      end
-    end
-
-    def try_grow
-      cap = @capacity << 1
-      items = @items.to_unsafe.realloc size: cap
-      unless items.null?
-        @items = Items(T, I).new items
-        @capacity = cap
-      end
-    end
-
-    def size
-      fast - slow
-    end
+  abstract struct RBQ
+    abstract def initialize
+    abstract def items
+    abstract def capacity
+    abstract def try_push
+    abstract def try_push_front
+    abstract def try_pop
+    abstract def try_grow
+    abstract def size
 
     alias PaddingT = UInt8[56]
 
@@ -204,6 +167,54 @@ module CSP
           @items.copy_from src + part, count: range.end
         end
       end
+    end
+  end
+
+  struct RBQ::Raw(T, I) < RBQ
+    def initialize(capacity_exponent)
+      @capacity = 1 << capacity_exponent
+      @mask = cap - 1
+      @items : Items(T, I) = Items(T, I).new self, @capacity
+    end
+
+    property items,
+      capacity : LibC::SizeT,
+      mask : LibC::SizeT,
+      slow = 0_u64,
+      fast = 0_u64
+
+    # Returns true if successful
+    def try_push(item : T) : Bool
+      !!if size < capacity
+        items[@slow] = item
+        @slow += 1
+      end
+    end
+
+    # Returns true if successful
+    def try_push_front(item : T) : Bool
+      !!if size < capacity
+        @items[@slow -= 1] = item
+      end
+    end
+
+    def try_pop : T?
+      if size > 0
+        @items[@slow].tap { @slow += 1 }
+      end
+    end
+
+    def try_grow
+      cap = @capacity << 1
+      items = @items.to_unsafe.realloc size: cap
+      unless items.null?
+        @items = Items(T, I).new items
+        @capacity = cap
+      end
+    end
+
+    def size
+      fast - slow
     end
   end
 
